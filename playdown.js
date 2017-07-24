9a926cc3d543112cda5516392b5f2947931488c5
Jarren Bird <jarrenmbird@gmail.com>
2017-07-24 11:54:56 -0600

    add filename specification functionality

--- playdown.js
+++ playdown.js
@@ -1,8 +1,11 @@
 // require 'fs' node module
 const fs = require('fs');
 
+// get the filename to be converted
+let playdownFilename = process.argv[2];
+
 // read the playdown file and store its contents in a variable
-const playdownFile = fs.readFileSync('examples/hello-world.txt', 'utf8');
+const playdownFile = fs.readFileSync(playdownFilename, 'utf8');
 
 // write an sml file from the contents of the file read
 fs.writeFile('playdown.html', playdownFile, function (err) {
