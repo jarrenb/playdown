a97d746ecc5265c141c0180e7ccb15d40fe9d246
Jarren Bird <jarrenmbird@gmail.com>
2017-07-24 11:43:11 -0600

    add reading and writing of files functionality

--- playdown.js
+++ playdown.js
@@ -1,2 +1,10 @@
-// read the specified file (.pd, .playdown, .txt are all accepted)
-console.log('hello playdown');
+// require 'fs' node module
+const fs = require('fs');
+
+// read the playdown file and store its contents in a variable
+const playdownFile = fs.readFileSync('examples/hello-world.txt', 'utf8');
+
+// write an sml file from the contents of the file read
+fs.writeFile('playdown.html', playdownFile, function (err) {
+  if (err) return console.log(err);
+});
