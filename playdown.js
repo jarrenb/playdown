2d748769c574d6661aab90baa2bd2710634bd0a5
Jarren Bird <jarrenmbird@gmail.com>
2017-07-25 11:57:38 -0600

    the beginning of an attempt at handling an asynchronous callback

--- playdown.js
+++ playdown.js
@@ -4,21 +4,20 @@ const fs = require('fs');
 // get the filename to be converted
 const getFilename = process.argv[2];
 
-// read the playdown file and store its contents in a variable
-// const playdownFile = fs.readFileSync(getFilename, 'utf8');
-// console.log(playdownFile);
-
-// make file data accessible via function
-function playdownFile(playdownData) {
-  fs.readFile(getFilename, 'utf8', (err, data) => {
-    if (err) throw playdownData(err);
-    playdownData(null, data);
+// I CAN PROBABLY DO THIS MUCH MORE EASILY, AND IT WOULD BE FINE TO DO IT THIS WAY, AS SYNCHRONOUSLY INSTEAD OF ASYNCHRONOUSLY. BUT I SUSPECT IN THE FUTURE I'LL HAVE TO KNOW HOW TO DEAL WITH STUFF ASYNCHRONOUSLY AND I WANT TO USE THIS AS AN OPPORTUNITY TO LEARN, RATHER THAN AVOID THE CONCEPT
+// get playdown file data
+function getPlaydownFile() {
+  // if the file is larger, this doesn't happen right away
+  fs.readFile(getFilename, function getFileData(err, data) {
+    if (err) throw err;
+    return data;
   });
 }
 
-playdownFile((err, data) => {
-  return data;
-});
+// contains the playdown file data
+function playdownFile() {
+
+}
 
 // write an sml file from the contents of the file read
 // fs.writeFile('playdown.html', playdownFile, function (err) {
